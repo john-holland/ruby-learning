7b0ffa4a84fb230e865b55b863ea8f93736dd5ee
John Holland <john.holland@veracross.com>
2019-11-07 22:02:12 -0500

    added main

---
+++ main.rb
@@ -0,0 +1,35 @@
+def tag(tagname, self_close=true)
+  define_method(tagname) do |attributes={}, &block|
+    markup = "<#{tagname}" + (attributes.empty? ? '' : ' ') + attributes.map { |k,v| "#{k}=\"#{v}\"" }.join(' ') + (block ? ">\n" : '')
+    # accepting a block and using the call method is the same as calling yield
+    #  with the exception that block is considered a parameter of the current block
+    #  and thus doesn't mess with enumeration ... weird and cool!
+    if block
+      bv = block.call
+      markup += if bv.kind_of?(Array)
+                  bv.join('\n')
+                else
+                  bv
+                end
+    end
+  ensure
+    markup += block ? "\n</#{tagname}>" : ' />'
+    return markup
+  end
+end
+
+tag('div')
+tag('p')
+tag('href')
+tag('input', self_close=true)
+
+url = 'google.com'
+markup = div :name => 'test' do
+  p do
+    [(href :src => url do 'test' end),
+     (input :type => 'text'),
+     (input :type => 'button', :value => 'press me!')]
+  end
+end
+
+puts markup
